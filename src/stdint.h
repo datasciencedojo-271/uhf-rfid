cd13aa6f08519f9bb5f52a9d62f27d0ac8e2fba5
finayat <finayat@pulsebiosciences.com>
2025-07-28 21:09:07 -0700

    - Build successful - Moved reverse-engineered code to rev_eng

---
+++ src/stdint.h
@@ -0,0 +1,16 @@
+#ifndef STDINT_H
+#define STDINT_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef unsigned char byte;
+typedef unsigned char bool;
+
+#define true 1
+#define false 0
+
+#endif
